b8ffc2fa9861f855a926531ec2fce7e931114e89
sharkdevs <mesharkz1@gmail.com>
2022-04-20 14:09:07 +0300

    setup npm

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,33 +1,25 @@
-  // Your code is going to go here
-  const Pet = (props)=>{
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, props.name),
-            React.createElement("h2", {}, props.animal),
-            React.createElement("h3", {}, props.breed),
-           ]
-      )
-  }
-  const App = ()=>{
-    return React.createElement(
-      "div",
-      {},
-      [
-          React.createElement("h1", {}, "Adopt Me"),
-          React.createElement(Pet,{
-              name: "Boby",
-              animal: "Dog",
-              breed: "German Shepherd"
-          }),
-          React.createElement(Pet,{
-            name: "Simba",
-            animal: "Dog",
-            breed: "Kawaida"
-        }),
-        ]
-    );
+// Your code is going to go here
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Boby",
+      animal: "Dog",
+      breed: "German Shepherd",
+    }),
+    React.createElement(Pet, {
+      name: "Simba",
+      animal: "Dog",
+      breed: "Kawaida",
+    }),
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
